8f7359e579c94601a8f1e2f3c5c91759ef4f21ff
AkemMensah <141195emmanuel@gmail.com>
2023-03-16 15:48:31 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
+#include <stdio.ih>
 /**
- * main - Entry level;
+ *  main - Entry level
  *
- * Return : 0 if successful.
+ * Return : Always 0 (successful)
  */
 int main(void)
 {
