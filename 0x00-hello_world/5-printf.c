9e43030ef68f88f59bdb14d9ac6f544524b1f915
AkemMensah <141195emmanuel@gmail.com>
2023-03-16 14:38:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - printing with the print funch.
+ * Return: 0 if succesful.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
