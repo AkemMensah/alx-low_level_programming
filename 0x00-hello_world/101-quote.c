23237dcf4186ad07ae71dbc11cc5d500135f3817
AkemMensah <141195emmanuel@gmail.com>
2023-03-16 16:43:03 -0700

    100-intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print quote
+ *
+ * Description: printing Dora's qoute
+ * Return: Always 1 (success)
+ */
+
+int main(void)
+{
+write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",59);
+retun (1);
+}
+
