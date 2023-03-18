86485ba18da4d3242326a7b5933ab07a0049206e
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 18:36:55 -0700

    reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+* main - This program will alphabets in reverse
+*
+* Description: using the mian function
+* this program prints lower case alphabets in reverse
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch  = 'z' ; ch <= 'a' ; ch--)
+{
+        putchar(ch);
+}
+printf('\n');
+return (0);
+}
