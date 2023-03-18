d8404431ab4b639114f6bac9fbcafaf49a43b232
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 18:06:50 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - This program will print all single digits
+*
+* Description: using the mian function
+* this program prints single digit numbers
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+for (i = 0 ; i < 10 ; i++)
+{
+	printf("%i", i);
+}
+  printf("\n");
+  return (0);
+}
