f8d245f21fd2d8043f004c9dd4425e3a76a2e94b
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 19:19:24 -0700

    12345

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - This program will print possible combinations
+*
+* Description: using the mian function
+* this program prints possible combintions of single digits
+* Return: 0
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 48 ; ch <= 57 ; ch++)
+{
+	putchar(ch);
+	if (ch !=57)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return (0);
+}
