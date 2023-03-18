bce6b80364e3414b7d5102a3fba83f400576d36c
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 17:18:03 -0700

    alphabelts

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+
+/**
+ * main - This program will assign a random number to the variable n each time it is executed
+ *
+ * Description: using the mian function
+ * this program prints "and is greater than 5, and is 0, and is less than 6 and not 0"
+ * Return: 0
+ */
+int main(void)
+{
+  char ch;
+
+  for (ch = 'a' ; ch <= 'z' ' ch++)
+  {
+	  putchar(ch);
+  }
+  for (ch = 'A' ; ch <= 'z' ; ch++)
+  {
+	  putchar(ch);
+  }
+  putchar('\n');
+  return (0);
+}
