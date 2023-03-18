fc4850f1d422b5172c6d968012ef7b65cd3a5a9f
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 17:34:48 -0700

    alphabelts

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,17 +8,15 @@
  * Return: 0
  */
 int main(void)
+{char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-  char ch;
-
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-  {
-	  putchar(ch);
-  }
-  for (ch = 'A' ; ch <= 'z' ; ch++)
-  {
-	  putchar(ch);
-  }
-  putchar('\n');
-  return (0);
+	putchar(ch);
+}
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
