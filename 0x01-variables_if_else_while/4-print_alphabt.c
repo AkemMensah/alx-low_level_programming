8e2ebccbca0dc4147cfae440c4e01f72f3f06c77
AkemMensah <141195emmanuel@gmail.com>
2023-03-17 17:48:57 -0700

    alphabelts

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This program will all alphabelts 
+ *
+ * Description: using the mian function
+ * this program prints except e and q
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'A' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+putchar(ch);
+}
+}
+putchar('\n');
+return (0);
+}
