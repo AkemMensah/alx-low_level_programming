6337998b86c8f4e23cb663c4ae86ac2a1cbacc60
Manuel <141195emmanuel@gmail.com>
2023-03-21 21:59:16 +0000

    useful files

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_0x10(void);
+int _islower(int c);
+int _isalphabet(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void times_table(void)[A[A[A[A[B[C
+i[D
+[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C
+[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_tabel(int n);
+void num(int r, int n);
