6e92224cd476768246fd599e11fc1f1f86e6d5b4
Manuel <141195emmanuel@gmail.com>
2023-03-21 22:19:19 +0000

    task zero

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,10 +9,7 @@ int _isalphabet(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void times_table(void)[A[A[A[A[B[C
-i[D
-[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C
-[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+void times_table(void);
 void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
