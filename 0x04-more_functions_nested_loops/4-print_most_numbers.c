15d624dd3386dc3f9653e19b04e2ab21f9f58204
Manuel <141195emmanuel@gmail.com>
2023-03-23 20:30:59 +0000

    tast 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most _numbers - prints numbers except 2 and 4
+ *
+ * Return: return nothing
+ */
+
+void print_most_numbers(void)
+{
+	int n=0;
+
+	for (; n <= 9; n++)
+	{
+	if (n == 2 || n == 4)
+	{
+	continue;
+	}
+	else
+	{
+	_putchar(n + '0');
+	}
+	}
+	_putchar('\n');
+}
+
