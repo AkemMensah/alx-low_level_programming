f776df3f3e9f1f8c7bb695e87016d0fba2fb7ac7
AkemMensah <141195emmanuel@gmail.com>
2023-03-28 16:56:14 -0700

    tasl 8 again

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,15 @@ void print_array(int *a, int n)
 
 	for (index_of_array = 0; index_of_array < n; index_of_array++)
 	{
-		printf("%d", a[index_of_array]);
+		/*printf("%d", a[index_of_array]);*/
 		if (index_of_array != (n - 1))
 		{
-			printf(", ");
+			/*printf(", ");*/
+			printf("%d, ", a[index_of_array]);
 		}
-		putchar('\n');
+		else
+			printf("%d", a[index_of_array]);
+		/*putchar('\n');*/
 	}
+	putcahr(10);
 }
