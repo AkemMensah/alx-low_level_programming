3b2407dc1fbfafaf2eea71f915e630e8945be558
AkemMensah <141195emmanuel@gmail.com>
2023-03-28 17:05:50 -0700

    tasks 8 again

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,15 +13,11 @@ void print_array(int *a, int n)
 
 	for (index_of_array = 0; index_of_array < n; index_of_array++)
 	{
-		/*printf("%d", a[index_of_array]);*/
+		printf("%d", a[index_of_array]);
 		if (index_of_array != (n - 1))
 		{
-			/*printf(", ");*/
-			printf("%d, ", a[index_of_array]);
+			printf(", ");
 		}
-		else
-			printf("%d", a[index_of_array]);
-		/*putchar('\n');*/
+		putchar('\n');
 	}
-	putcahr(10);
 }
