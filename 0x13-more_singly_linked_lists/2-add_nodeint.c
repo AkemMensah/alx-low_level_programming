ca10f3974b2d3103d90f2edbf59e169ae874c34d
AkemMensah <141195emmanuel@gmail.com>
2023-05-02 09:37:14 -0700

    task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node to the end of a linked list
+ * add_nodeint - adds a node to the end of a linked list
  * @head: points to the first element in the list
  * @n: data to be inserted in the new node
  *
